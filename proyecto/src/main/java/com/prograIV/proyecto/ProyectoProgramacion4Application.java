7247b3e3eb99fa0cbcc2fa54235fd69339536f08
Romeo Melgar <lm0071032018@unab.edu.sv.com>
2020-10-24 15:59:43 -0600

    commit prueba

---
+++ proyecto/src/main/java/com/prograIV/proyecto/ProyectoProgramacion4Application.java
@@ -0,0 +1,13 @@
+package com.prograIV.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoProgramacion4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoProgramacion4Application.class, args);
+	}
+
+}
---
+++ proyecto/src/test/java/com/prograIV/proyecto/ProyectoProgramacion4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prograIV.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoProgramacion4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
